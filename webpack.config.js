d03a6ac183675e47868c4b5705f04a895dc5982a
Suranart Niamcome <suranartnc@gmail.com>
2016-02-29 16:13:42 +0700

    implement webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ var webpack = require('webpack');
 module.exports = {
 
     devtool: 'cheap-module-eval-source-map',
-
-    entry: path.join(__dirname, 'src/client.js'),
     
     entry: [
         // 'eventsource-polyfill', // necessary for hot reloading with IE
