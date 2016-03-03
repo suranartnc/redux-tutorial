4b7028eba2cfd18ed6c094ac53208965eeada21f
SuranartN <suranartnc@gmail.com>
2016-03-03 22:55:22 +0700

    add simple redux app

--- src/client.js
+++ src/client.js
@@ -1,6 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createStore } from 'redux';
 
-import App from './App'
+// Reducer
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + action.size
+        case 'DECREMENT':
+            return state - action.size
+        default:
+            return state
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// Store
+const store = createStore(reducer);
+
+// Subscribe to the updates
+store.subscribe(function() {
+    console.log(store.getState());
+});
+
+// Dispatch some actions
+store.dispatch({ 
+	type: 'INCREMENT',
+	size: 1
+});
+
+store.dispatch({ 
+	type: 'INCREMENT',
+	size: 5 
+});
+
+store.dispatch({ 
+	type: 'DECREMENT',
+	size: 1
+});
